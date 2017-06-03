d437bf795c419085882951c5388b97d925c5f320
EdwardLin2013 <edward_lin@mymail.sutd.edu.sg>
2017-06-03 15:58:17 +0800

    Update IBMBSS.m

--- 04_iKalaProject/20_01_AnnoBSS/IBMBSS.m
+++ 04_iKalaProject/20_01_AnnoBSS/IBMBSS.m
@@ -8,30 +8,18 @@ WavDirStr = '../../03_Database/iKala/Wavfile/';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Step 0 - Addpath for SineModel/UtilFunc/BSS_Eval
 addpath(genpath(ToolDirStr));
-%% Step 0 - Parmaters Setting
-% STFT
-Parm.M = 1024;                  % Window Size, 23.22ms
-Parm.window = hann(Parm.M);     % Window in Vector Form
-Parm.N = 1024;                  % Analysis FFT Size, 23.22ms
-Parm.H = 512;                   % Hop Size, 11.61ms
-Parm.fs = 44100;                % Sampling Rate, 44.10K Hz
-Parm.numBins = 513;
-Parm.numFrames = 2584;
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Step 0 - Obtain Audio File Name
 WavFileNames = iKalaWavFileNames(WavDirStr);
 numMusics = numel(WavFileNames);
 %% Step 0 - Parmaters Setting
 % STFT
-Parm.M = 1024;                  % Window Size, 46.44ms
+Parm.M = 1024;                  % Window Size, 23.22ms
 Parm.window = hann(Parm.M);     % Window in Vector Form
-Parm.N = 1024;                  % Analysis FFT Size, 46.44ms
-Parm.H = 512;                   % Hop Size, 23.22ms
+Parm.N = 1024;                  % Analysis FFT Size, 23.22ms
+Parm.H = 256;                   % Hop Size, 5.81ms
 Parm.fs = 44100;                % Sampling Rate, 44.10K Hz
-Parm.numBins = 513;
-Parm.numFrames = 2584;
-Parm.FreqCrit = 1:25;
+Parm.FreqCrit = 1:HzToFBin(1000,Parm);
 Parm.numTFBin = 5;
 trainMusic = [3,4,5,6,7,8,9,10,11,13,14,16,17,20,21,23,27,29,30,33,34,35,36,37,38,39,41,44,46,50,52,53,54,55,56,57,59,60,61,63,64,65,66,67,70,71,73,77,78,82,84,85,86,88,91,92,95,96,97,98,102,103,108,109,114,115,116,117,119,123,124,126,127,128,130,133,141,142,144,145,146,147,149,150,151,153,154,155,156,158,159,160,162,163,164,165,169,170,172,173,174,176,180,184,185,186,187,188,189,190,191,193,194,196,197,199,200,201,203,207,208,209,210,211,215,216,217,219,221,224,225,226,227,230,231,232,233,234,235,236,237,238,239,240,241,242,246,248,249,250,251,252];
 valMusic = [1,22,28,32,42,48,49,58,62,72,74,80,83,89,90,93,101,106,120,121,122,125,129,131,136,140,143,148,157,161,168,178,181,182,183,192,195,198,205,206,212,213,214,220,222,229,243,244,245,247];
